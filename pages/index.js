d648f53cd009491d2996f13c109b513ab39d239b
omgitsfrancis <omgitsfrancis@gmail.com>
2020-04-01 16:06:11 -0700

    Added patrick and speech bubble to application.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 import Button from "react-bootstrap/Button";
+import SpeechBubble from "../components/SpeechBubble";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  max-width: 1366px;
+  margin: 0 auto;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const PatrickImg = styled.img`
+  height: 60vh;
+  transform: translateX(-10px);
+`;
+
+const StyledSpeechBubble = styled(SpeechBubble)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: auto;;
+  
+
+`;
+
+const QuoteText = styled.span`
+  text-align: center;
 `;
 
 export default function Index() {
@@ -28,15 +53,18 @@ export default function Index() {
 
   return (
     <Container>
-      <h1>Patrick Quotes</h1>
+      <h1 style={{textAlign: "center"}}>Patrick Quotes</h1>
       {error ? (
         <p>Error has occured</p>
       ) : (
-        <>
-          <p>{loading ? "LOADING..." : quote}</p>
-          <Button onClick={() => fetchQuote()}>Get Quote</Button>
-        </>
+        <Wrapper>
+          <PatrickImg src="patrick.png" />
+          <StyledSpeechBubble color="#FFFF99">
+            <QuoteText>{loading ? "LOADING..." : quote}</QuoteText>
+          </StyledSpeechBubble>
+        </Wrapper>
       )}
+      <Button disabled={loading} onClick={() => fetchQuote()}>Get Quote</Button>
     </Container>
   );
 }
